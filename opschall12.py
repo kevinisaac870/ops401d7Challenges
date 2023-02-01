867e3d015c778a0618cdb9b53a024ff145384dfe
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-01-31 20:13:14 -0700

    Create opschall12.py

---
+++ opschall12.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 12
+# Author:                       Kevin Isaac
+# Date of latest revision:      20230131
+# Purpose:      Ops Challenge: Network Security Tool with Scapy Part 2 of 3
+
+# Under Construction
