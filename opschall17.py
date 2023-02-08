37e872ba31d3a88a9511539c36927474029ef8c8
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-02-07 22:15:04 -0700

    Create opschall17.py

---
+++ opschall17.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 17
+# Author:                       Kevin Isaac
+# Date of latest revision:      20230207
+# Purpose:        
+
+# Attribution: Marco, Zoom video and classmates.
+
+UNDER CONSTRUCTION
