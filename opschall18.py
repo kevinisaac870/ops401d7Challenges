266f62a34e6491c00cdad0754a29ed04e08866d5
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-02-08 21:55:15 -0700

    Create opschall18.py

---
+++ opschall18.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 18
+# Author:                       Kevin Isaac
+# Date of latest revision:      202300208
+# Purpose:        
+
+UNDER CONSTRUCTION - I NEED TO TALK TO YOU STILL ABOUT FIGURING THIS OUT
