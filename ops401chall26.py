9aae24390ee56c47d9fadd22735f26dd3df872eb
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-02-20 22:23:57 -0700

    Create ops401chall26.py

---
+++ ops401chall26.py
@@ -0,0 +1,9 @@
+
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 26
+# Author:                       Kevin Isaac
+# Date of latest revision:      20230220
+# Purpose:        
+
+I will get with Ben and work with him one-on-one as he's available to gwt caught up.
