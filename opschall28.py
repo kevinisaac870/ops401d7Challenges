8a9a60241a3243e1152eb83c9c1a52e16d328cf5
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-02-26 14:58:46 -0700

    Create opschall28.py

---
+++ opschall28.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 28
+# Author:                       Kevin Isaac
+# Date of latest revision:      20230222
+# Purpose:        
+
+I will get with Ben and work with him one-on-one as he's available to gwt caught up.
