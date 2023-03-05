a4f4ac76eebe6fab4229dcf627222e540866bab2
Kevin Isaac <116570400+kevinisaac870@users.noreply.github.com>
2023-03-05 10:17:54 -0700

    Create opschall30.py

---
+++ opschall30.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Script:                       Ops Challenge 30
+# Author:                       Kevin Isaac
+# Date of latest revision:      2023028
+# Purpose:        
+
+I will get with Ben and work with him one-on-one as he's available to get caught up.
